033daf0980b468397b83f1e8ae78a87510e8b7dd
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-05-07 15:09:51 +1000

    update

---
+++ pages/copy.bat
@@ -0,0 +1,3 @@
+xcopy %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\apps\crc-stream-b-extension\pages\*.* %USERPROFILE%\Documents\GitHub\woolcock-imr\crc-stream-b-extension\pages\*.* /S /D
+
+pause
